a58ef162c7663204839b133e8a70b7bf83eb11f7
arielclasicoliberalg-tech <arielclasicoliberalg@gmail.com>
2026-05-22 08:31:01 -0400

    Update 1_✍️_LLENAR_MIS_PRONOSTICOS.py

--- pages/1_✍️_LLENAR_MIS_PRONOSTICOS.py
+++ pages/1_✍️_LLENAR_MIS_PRONOSTICOS.py
@@ -93,6 +93,14 @@ label[data-testid="stWidgetLabel"] p {
             if st.button("🔄 Refrescar"):
                 st.rerun()
         else:
+            st.markdown("""
+<style>
+div[data-testid="stNumberInput"] label p {
+    font-size: 30px !important;
+    font-weight: bold;
+}
+</style>
+""", unsafe_allow_html=True)
             with st.form(key=f"form_{partido['id']}"):
                 c1, c2, c3 = st.columns([1, 0.5, 1])
                 g1 = c1.number_input(f"Goles {partido['equipo_1']}", min_value=0, max_value=20, step=1)
