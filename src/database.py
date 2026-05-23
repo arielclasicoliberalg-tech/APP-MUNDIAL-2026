6a5198b5f985fc761600e74486f1dcaa25ad190c
arielclasicoliberalg-tech <arielclasicoliberalg@gmail.com>
2026-05-23 08:38:20 -0400

    Update database.py

--- src/database.py
+++ src/database.py
@@ -7,4 +7,11 @@ def get_supabase_client():
     key = st.secrets["SUPABASE_KEY"]
     return create_client(url, key)
 
+@st.cache_resource
+def get_supabase_admin():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_SERVICE_KEY"]
+    return create_client(url, key)
+
 supabase = get_supabase_client()
+supabase_admin = get_supabase_admin()
