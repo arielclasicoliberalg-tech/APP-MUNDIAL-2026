0ec6742542668e457a26c679353a4907db1e7c22
arielclasicoliberalg-tech <arielclasicoliberalg@gmail.com>
2026-05-30 23:51:43 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+import pytz
 
 def es_fecha_valida(fecha_partido, hora_partido):
-    # Combina fecha y hora y compara con el momento actual
     fecha_str = f"{fecha_partido} {hora_partido}"
     dt_partido = datetime.strptime(fecha_str, '%Y-%m-%d %H:%M:%S')
-    return datetime.now() < dt_partido
+    
+    # Zona horaria Bolivia
+    tz_bolivia = pytz.timezone('America/La_Paz')
+    ahora_bolivia = datetime.now(tz_bolivia).replace(tzinfo=None)
+    
+    return ahora_bolivia < dt_partido
